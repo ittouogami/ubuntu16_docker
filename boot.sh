241bc0c5d5ac25401fe8b7e9fc1febd8ca891024
ittou <VYG07066@gmail.com>
2019-03-03 23:22:57 +0900

    -v .Xauthority deleted

--- boot.sh
+++ boot.sh
@@ -5,7 +5,6 @@ docker run -ti --rm \
     -e USER=$USER \
     -e UART_GROUP_ID=20 \
     -e DISPLAY=$DISPLAY \
-    -v $HOME/.Xauthority:/root/.Xauthority \
     -v /tmp/.X11-unix/:/tmp/.X11-unix \
     -v /home/hara/docker/hara:/home/hara \
     ubuntu16 /bin/bash
